b45c056d73f7e1d976c577b102509bce8fa2dde3
SYAM107u8248 <syam107u8248@ad.pcc.edu>
2019-10-24 16:04:05 -0700

    add week 5 lab notes and sample function files

--- week5/call_API.py
+++ week5/call_API.py
@@ -1,6 +1,20 @@
 # call_API.py
+import pandas as pd
 
-def call_API(w_type,n_data_pts):
-    pass
-
+def call_API(weather_type, num_data_pts):
+    # df = pd.read_csv(‘https://api.thingspeak.com/channels/12397/fields/4.csv?results=20’)
+    
+    # create the url out of a couple different strings
+    url1 = 'https://api.thingspeak.com/channels/12397/fields/'
+    w_select_dict = {'temperature':4,'humidity':5}
+    
+    url2 = str(w_select_dict[weather_type])
+    url3 = '.csv?results='
+    url4 = str(num_data_pts)
+    
+    url = url1 + url2 + url3 + url4
+    # call the url to create the dataframe
+    df = pd.read_csv(url)
+    
+    # return a dataframe with the data
     return df
--- week5/clean_data.py
+++ week5/clean_data.py
@@ -1,6 +1,8 @@
 # clean_data.py
 
-def clean_data(df):
-    pass
-
-    return clean_data_array
+def clean_data(df, w_type_str):
+    
+    col = df['field4']
+    array_of_floats = np.array(col)
+    
+    return array_of_floats
